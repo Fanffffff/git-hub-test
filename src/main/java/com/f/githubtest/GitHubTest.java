d1bddf661eef914aaaa3d2e05fc2b12e45d92084
fan <1243648205@qq.com>
2022-06-12 11:59:55 +0800

    hot-fix commit2

--- src/main/java/com/f/githubtest/GitHubTest.java
+++ src/main/java/com/f/githubtest/GitHubTest.java
@@ -12,5 +12,7 @@ public class GitHubTest {
         System.out.println("hello,no");
         System.out.println(333);
         System.out.println(444);
+        System.out.println(444);
+        System.out.println(444);
     }
 }
