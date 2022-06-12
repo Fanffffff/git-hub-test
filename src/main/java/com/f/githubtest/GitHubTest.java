9ece4a577e8717a5395518492f7db958f2445aa6
fan <1243648205@qq.com>
2022-06-12 11:21:51 +0800

    第一次提交

---
+++ src/main/java/com/f/githubtest/GitHubTest.java
@@ -0,0 +1,11 @@
+package com.f.githubtest;
+
+/**
+ * @author shkstart
+ * @create2022-06-1211:19
+ */
+public class GitHubTest {
+    public static void main(String[] args) {
+        System.out.println(1+1);
+    }
+}
